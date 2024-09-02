0b3ba4de6c8617d0ea19c31730919df26c4d4f78
Halis Utku Aladağ <halisutku.aladag@otelz.com>
2024-09-02 09:45:39 +0300

    1.0.5 Hotfix release version

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ async function get_session_id(team_spkey, version) {
     const get_session_body = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({'team_key': team_spkey,'platform': platform,'version': version})
+        body: JSON.stringify({ 'team_key': team_spkey, 'platform': platform, 'version': version })
     }
     const response = await fetch(api_url + '/get_automation_session/cypress', get_session_body);
     const responseData = await response.json();
@@ -45,30 +45,31 @@ async function save_test_results(session_id, results, team_spkey) {
         const save_test_result_body = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({'session_id': session_id,'test_name': `${results.tests[i].title[0]} - ${results.tests[i].title[1]}`,'test_result': result,'error_log': errorValue})
+            body: JSON.stringify({ 'session_id': session_id, 'test_name': `${results.tests[i].title[0]} - ${results.tests[i].title[1]}`, 'test_result': result, 'error_log': errorValue })
         }
         await fetch(api_url + '/save-test-result/cypress', save_test_result_body)
             .then(response => response.json())
             .then(response_data => {
-                var filePath = results.screenshots[0].path.replace(/\\/g, '/').replace(/\/$/, '');
-                console.log("Burada path attım hadi bakalımm...")
-                console.log(filePath)
-                fs.readFile(filePath, (err, data) => {
-                    var test_id = response_data.test_id
-                    var base64Data = data.toString('base64');
+                if (results.screenshots[0].path) {
+                    var filePath = results.screenshots[0].path.replace(/\\/g, '/').replace(/\/$/, '');
+                    fs.readFile(filePath, (err, data) => {
+                        var test_id = response_data.test_id
+                        var base64Data = data.toString('base64');
+                        fetch(api_url + "/upload-test-screenshot/cypress", {
+                            method: "POST",
+                            headers: { "Content-Type": "application/json" },
+                            body: JSON.stringify({ 'files': base64Data, 'team_spkey': team_spkey, 'session': session_id, 'test_id': test_id })
+                        }).then(response => response.json()).then(result => {
+                            console.log('Success:', result);
+                        }).catch(error => {
+                            console.error('Error:', error);
+                        });
+                    })
+                }
 
-                    fetch(api_url + "/upload-test-screenshot/cypress", {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({'files' : base64Data,'team_spkey': team_spkey,'session': session_id,'test_id': test_id})
-                    }).then(response => response.json()).then(result => {
-                        console.log('Success:', result);
-                    }).catch(error => {
-                        console.error('Error:', error);
-                    });
-                })
             }
-        );
+
+            );
     }
 }
 
@@ -76,7 +77,7 @@ async function complete_test_session(session_id) {
     const stop_session_body = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({'session_id': session_id})
+        body: JSON.stringify({ 'session_id': session_id })
     }
     await fetch(api_url + '/stop-automation-session/cypress', stop_session_body);
 }
